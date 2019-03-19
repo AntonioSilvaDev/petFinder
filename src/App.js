e3dfded70ae74164c7118347d952608283c6f1b1
TonyS <tonysilvasd@gmail.com>
2019-03-18 17:17:52 -0700

    adds Results.js component with renders API call data

--- src/App.js
+++ src/App.js
@@ -1,70 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import pf from "petfinder-client";
-import Pet from "./Pet";
-
-const petfinder = pf({
-  //API(client) Library that btholt created for this workshop
-  secret: process.env.API_SECRET,
-  key: process.env.API_KEY
-});
+import Results from "./Results";
 
 class App extends React.Component {
   //class component
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: {}
-    };
-    //constructors are run first, then component mounts
-  }
-
-  componentDidMount() {
-    //lifecycle method
-    petfinder.pet
-      .find({ output: "full", location: "San Diego, CA" })
-      .then(data => {
-        let pets;
-        console.log("response data", data.petfinder.pets.pet);
-
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-      });
-  }
-
   render() {
     return (
       <div>
-        <h1>Adopt Me Please</h1>
-        <Pet
-          animal="Dog"
-          name="Tom"
-          breed="German Shepard"
-          likes="Long Walks"
-          dislikes="When no one plays with him"
-        />
-        <Pet
-          animal="Dog"
-          name="Bill"
-          breed="Sheep Dog"
-          likes="Long Walks"
-          dislikes="When no one plays with him"
-        />
-        <Pet
-          animal="Dog"
-          name="Charlie"
-          breed="Chihuahua"
-          likes="Long Walks"
-          dislikes="When no one plays with him"
-        />
+        <h1>Adopt Me Please!</h1>
+        <Results />
       </div>
     );
   }
--- src/Pet.js
+++ src/Pet.js
@@ -1,24 +1,53 @@
 import React from "react";
 
-const Pet = props => {
-  //functional component
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h5", {}, props.animal),
-  //     React.createElement("h2", {}, props.name),
-  //     React.createElement("h2", {}, props.breed),
-  //     React.createElement("h3", {}, props.likes),
-  //     React.createElement("h4", {}, props.dislikes)
-  //   ]);
+// const Pet = props => {
+//functional component
+//   return React.createElement("div", {}, [
+//     React.createElement("h5", {}, props.animal),
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h2", {}, props.breed),
+//     React.createElement("h3", {}, props.likes),
+//     React.createElement("h4", {}, props.dislikes)
+//   ]);
 
-  return (
-    <div>
-      <h1>{props.animal}</h1>
-      <h2>{props.name}</h2>
-      <h3>{props.breed}</h3>
-      <h4>{props.likes}</h4>
-      <h5>{props.dislikes}</h5>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Type: {props.animal}</h1>
+//       <h2>Name: {props.name}</h2>
+//       <h3>Breed: {props.breed}</h3>
+//       <h4>Description: {props.description}</h4>
+//       <h5>Id is: {props.id}</h5>
+//     </div>
+//   );
+// };
+
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    let imgSrc = `http://placecorgi.com/300/300`;
+    if (photos[0] && photos[0].value) {
+      imgSrc = photos[0].value;
+    }
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={imgSrc} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pet;
